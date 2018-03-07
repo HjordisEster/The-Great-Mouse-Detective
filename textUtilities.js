aa54dd41f76702d914f1a384c572e575cd408ae7
Hjordis Gudjonsdottir <hjordis.ester@gmail.com>
2018-03-07 17:28:06 +0000

    title

--- textUtilities.js
+++ textUtilities.js
@@ -1,3 +1,9 @@
 var expect = require('chai').expect;
 
 expect(true).to.be.true;
+
+function titleCace(title){
+    return title;
+}
+
+expect(titleCace('the great mouse detective')).to.be.a('string');
